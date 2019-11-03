9c207fc6af9aacff27a198e5bb5f07f39a03e4b1
T.Oka <chita123daa@gmail.com>
2019-11-04 04:54:19 +0900

    Appのリンクと修正　works アプリのリンク

--- myport/myport/src/App.vue
+++ myport/myport/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app class="app">
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
         <span>Menu</span>
@@ -7,16 +7,28 @@
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
-        flat
+        text
         to="/"
       >
         <span class="mr-2">Home</span>
       </v-btn>
       <v-btn
-        flat
+        text
         to="/about"
       >
         <span class="mr-2">About</span>
+      </v-btn>
+      <v-btn
+        text
+        to="/works"
+      >
+        <span class="mr-2">Works</span>
+      </v-btn>
+      <v-btn
+        text
+        to="/contact"
+      >
+        <span class="mr-2">Contact</span>
       </v-btn>
     </v-app-bar>
 
--- myport/myport/src/views/Works.vue
+++ myport/myport/src/views/Works.vue
@@ -5,11 +5,46 @@
       wrap
     >
       <v-flex mb-4>
-        <a class="body-1 font-weight-bold mb-3">
+        <h1 class="display-2 font-weight-bold mb-3">
           Works
-        </a>
+        </h1>
       </v-flex>
+      <v-flex
+        mb-5
+        xs12
+      >
+        <h2 class="headline font-weight-bold mb-3">Used Flameworks 1.vue.js 2.ruby on rails</h2>
 
+
+        <v-layout justify-center>
+          <a
+            v-for="(eco, i) in ecosystem"
+            :key="i"
+            :href="eco.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ eco.text }}
+          </a>
+        </v-layout>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
+<script>
+export default {
+  data: () => ({
+    ecosystem: [
+      {
+        text: '1.vue-slot-app',
+        href: 'https://chitachita.github.io/vue-slot-app/#/',
+      },
+      {
+        text: '2.honda-survey',
+        href: 'https://honda-survey.herokuapp.com',
+      },
+    ],
+    
+  }),
+};
+</script>
