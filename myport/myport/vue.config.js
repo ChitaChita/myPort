9b8c7662f5a17004af75292ff442b1dd5144d341
T.Oka <chita123daa@gmail.com>
2019-11-03 18:58:09 +0900

    vuetify

--- myport/myport/vue.config.js
+++ myport/myport/vue.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  // options...
-  devServer: {
-      watchOptions: {
-        ignored: /node_modules/,
-        aggregateTimeout: 300,
-        poll: 1000,
-      },
-  }
+  "devServer": {
+    "watchOptions": {
+      "ignored": {},
+      "aggregateTimeout": 300,
+      "poll": 1000
+    }
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
